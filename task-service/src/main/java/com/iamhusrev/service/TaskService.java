13cc209bb23f34791b0cc7f36a9268c2ec1552a9
iamhusrev <iamhusrev@gmail.com>
2026-01-28 18:04:15 +0300

    task project refactor

--- task-service/src/main/java/com/iamhusrev/service/TaskService.java
+++ task-service/src/main/java/com/iamhusrev/service/TaskService.java
@@ -2,31 +2,133 @@ package com.iamhusrev.service;
 
 import com.iamhusrev.dto.ProjectDTO;
 import com.iamhusrev.dto.TaskDTO;
+import com.iamhusrev.dto.UserResponseDTO;
+import com.iamhusrev.entity.Project;
+import com.iamhusrev.entity.Task;
 import com.iamhusrev.entity.User;
 import com.iamhusrev.enums.Status;
+import com.iamhusrev.respository.TaskRepository;
+import com.iamhusrev.util.MapperUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface TaskService {
+@Service
+@RequiredArgsConstructor
+public class TaskService {
 
-    TaskDTO findById(Long id);
-    List<TaskDTO> listAllTasks();
-    void save(TaskDTO dto);
-    void update(TaskDTO dto);
-    void delete(Long id);
-    int totalNonCompletedTask(String projectCode);
-    int totalCompletedTask(String projectCode);
+    private final TaskRepository taskRepository;
+    private final MapperUtil mapperUtil;
+    private final UserClientService userClientService;
 
-    void deleteByProject(ProjectDTO project);
 
-    void completeByProject(ProjectDTO project);
+    public TaskDTO findById(Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        return task.map(value -> mapperUtil.convert(value, new TaskDTO())).orElse(null);
+    }
 
-    List<TaskDTO> listAllTasksByStatusIsNot(Status status);
+    public List<TaskDTO> listAllTasks() {
+        List<Task> list = taskRepository.findAll();
+        return list.stream().map(obj -> mapperUtil.convert(obj, new TaskDTO())).collect(Collectors.toList());
+    }
 
-    void updateStatus(TaskDTO task);
+    public void save(TaskDTO dto) {
+        dto.setTaskStatus(Status.OPEN);
+        dto.setAssignedDate(LocalDate.now());
+        Task task = mapperUtil.convert(dto, new Task());
+        taskRepository.save(task);
+    }
 
-    List<TaskDTO> listAllTasksByStatus(Status status);
+    public void update(TaskDTO dto) {
 
-    List<TaskDTO> readAllByAssignedEmployee(User assignedEmployee);
+        Optional<Task> task = taskRepository.findById(dto.getId());
+        Task convertedTask = mapperUtil.convert(dto, new Task());
+
+        if (task.isPresent()) {
+            convertedTask.setId(task.get().getId());
+            convertedTask.setTaskStatus(dto.getTaskStatus() == null ? task.get().getTaskStatus() : dto.getTaskStatus());
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
+
+    }
+
+    public void delete(Long id) {
+
+        Optional<Task> foundTask = taskRepository.findById(id);
+
+        if (foundTask.isPresent()) {
+            foundTask.get().setIsDeleted(true);
+            taskRepository.save(foundTask.get());
+        }
+
+
+    }
+
+    public int totalNonCompletedTask(String projectCode) {
+        return taskRepository.totalNonCompletedTasks(projectCode);
+    }
+
+    public int totalCompletedTask(String projectCode) {
+        return taskRepository.totalCompletedTasks(projectCode);
+    }
+
+    public void deleteByProject(ProjectDTO project) {
+        List<TaskDTO> list = listAllByProject(project);
+        list.forEach(taskDTO -> delete(taskDTO.getId()));
+    }
+
+    public void completeByProject(ProjectDTO project) {
+        List<TaskDTO> list = listAllByProject(project);
+        list.forEach(taskDTO -> {
+            taskDTO.setTaskStatus(Status.COMPLETE);
+            update(taskDTO);
+        });
+    }
+
+    public List<TaskDTO> listAllTasksByStatusIsNot(Status status) {
+
+        UserResponseDTO userResponseDto = (UserResponseDTO) userClientService.getUserByUserName("john@employee.com").getData();
+
+        User loggedInUser = mapperUtil.convert(userResponseDto.getData(), new User());
+
+        List<Task> list = taskRepository.findAllByTaskStatusIsNotAndAssignedEmployee(status, loggedInUser);
+        return list.stream().map(obj -> mapperUtil.convert(obj, new TaskDTO())).collect(Collectors.toList());
+    }
+
+    public void updateStatus(TaskDTO dto) {
+
+        Optional<Task> task = taskRepository.findById(dto.getId());
+
+        if (task.isPresent()) {
+            task.get().setTaskStatus(dto.getTaskStatus());
+            taskRepository.save(task.get());
+        }
+
+    }
+
+    public List<TaskDTO> listAllTasksByStatus(Status status) {
+        UserResponseDTO userResponseDto = (UserResponseDTO) userClientService.getUserByUserName("john@employee.com").getData();
+
+        User loggedInUser = mapperUtil.convert(userResponseDto.getData(), new User());
+
+        List<Task> list = taskRepository.findAllByTaskStatusAndAssignedEmployee(status, loggedInUser);
+        return list.stream().map(obj -> mapperUtil.convert(obj, new TaskDTO())).collect(Collectors.toList());
+    }
+
+    public List<TaskDTO> readAllByAssignedEmployee(User assignedEmployee) {
+        List<Task> list = taskRepository.findAllByAssignedEmployee(assignedEmployee);
+        return list.stream().map(obj -> mapperUtil.convert(obj, new TaskDTO())).collect(Collectors.toList());
+    }
+
+    private List<TaskDTO> listAllByProject(ProjectDTO project) {
+
+        List<Task> list = taskRepository.findAllByProject(mapperUtil.convert(project, new Project()));
+        return list.stream().map(obj -> mapperUtil.convert(obj, new TaskDTO())).collect(Collectors.toList());
+    }
 
 }
